5e4a53b2114b88931a1fec7ee5e89d70871b110a
Elon-Mao <elonmao.engineer@gmail.com>
2024-02-15 09:41:49 +0800

    fix loading bug

--- src/common/customPromise.ts
+++ src/common/customPromise.ts
@@ -35,21 +35,3 @@ export default (promise: Promise<any>) => {
     })
   })
 }
-
-// export const customAsync = async (func: Function) => {
-//   const systemStore = useSystemStore()
-//   systemStore.setLoading(true)
-//   const timeoutId = setTimeout(() => {
-//     throw new Error('Timed out')
-//   }, 8000)
-//   try {
-//     const result = await func()
-//     clearTimeout(timeoutId);
-//     systemStore.setLoading(false)
-//     return result
-//   } catch(e) {
-//     systemStore.setLoading(false)
-//     showNotify('Network Error')
-//     throw e
-//   }
-// }
